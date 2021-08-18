b0a4041bcd20917b1e6004eb93acd808b4ba5d89
Nguyễn Mạnh Thế <80904359+the281200@users.noreply.github.com>
2021-08-18 09:21:08 +0700

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <center><h2>Danh sach \f sinh vien</h2></center>
+    <center><h2>Danh sach sinh vien</h2></center>
     <form method="post" action="addStudent.php" style="margin-left:540px" >
                 <button type="submit"  class="btn btn-primary" >Them sinh vien</button>
     </form>
